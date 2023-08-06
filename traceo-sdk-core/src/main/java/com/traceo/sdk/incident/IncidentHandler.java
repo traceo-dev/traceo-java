c6d5af67cb129bf46a25fc795e3f220eb626215d
Piotrek <p.szewczyk-98@wp.pl>
2023-08-06 13:45:11 +0200

    fix/parse exception traces

--- traceo-sdk-core/src/main/java/com/traceo/sdk/incident/IncidentHandler.java
+++ traceo-sdk-core/src/main/java/com/traceo/sdk/incident/IncidentHandler.java
@@ -1,12 +1,13 @@
 package com.traceo.sdk.incident;
 
-import com.traceo.sdk.DefaultRequest;
-import com.traceo.sdk.EventCallback;
-import com.traceo.sdk.TraceoIncident;
-import com.traceo.sdk.TraceoRuntimePlatform;
+import com.traceo.sdk.*;
 import com.traceo.sdk.http.HttpAsyncClient;
 import com.traceo.sdk.logging.ClientLogger;
 import com.traceo.sdk.utils.ThrowableUtils;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.traceo.sdk.client.CoreClient.getConfigs;
 
@@ -60,9 +61,49 @@ public class IncidentHandler {
             traceoIncident.setMessage(throwable.getMessage());
         }
 
+        List<TraceoTrace> traces = getTraceList(throwable);
+        traceoIncident.setTraces(traces);
+
         traceoIncident.setStack(stacktrace);
         traceoIncident.setPlatform(platform);
 
         return traceoIncident;
+    }
+
+    private static List<TraceoTrace> getTraceList(Throwable throwable) {
+        List<TraceoTrace> traces = new ArrayList<>();
+
+        for (StackTraceElement stackTraceElement : throwable.getStackTrace()) {
+            TraceoTrace trace = new TraceoTrace();
+            trace.setFunction(stackTraceElement.getMethodName());
+
+            String[] splitFilename = stackTraceElement.getFileName().split("\\.");
+            if (splitFilename.length > 1) {
+                trace.setFilename(splitFilename[0]);
+                trace.setExtension(splitFilename[1]);
+            }
+
+            trace.setLineNo(stackTraceElement.getLineNumber());
+            trace.setAbsPath(stackTraceElement.getClassName());
+
+            traces.add(trace);
+        }
+        return traces;
+    }
+
+    private static String getAbsolutePath(String className) {
+        try {
+            Class<?> myClass = Class.forName(className);
+            ClassLoader classLoader = myClass.getClassLoader();
+
+            URL classResource = classLoader.getResource(className.replace('.', '/') + ".class");
+            if (classResource == null) {
+                return null;
+            }
+
+            return classResource.getPath();
+        } catch (java.lang.ClassNotFoundException e) {
+            return null;
+        }
     }
 }
